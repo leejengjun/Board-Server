c5e99a947631bfa182ebc46b41de2cd2e279d7e4
GIANTSTEPCORP\jeongjun.lee <youngwoo4215@naver.com>
2024-01-18 14:50:22 +0900

    #10 댓글, 태그 기능 개발
    - 해시태그 기능 개발
    - 댓글 및 대댓글 기능 개발
    - 데모 및 테스트

--- src/main/java/com/fastcampus/boardserver/service/impl/PostServiceImpl.java
+++ src/main/java/com/fastcampus/boardserver/service/impl/PostServiceImpl.java
@@ -1,8 +1,12 @@
 package com.fastcampus.boardserver.service.impl;
 
+import com.fastcampus.boardserver.dto.CommentDTO;
 import com.fastcampus.boardserver.dto.PostDTO;
+import com.fastcampus.boardserver.dto.TagDTO;
 import com.fastcampus.boardserver.dto.UserDTO;
+import com.fastcampus.boardserver.mapper.CommentMapper;
 import com.fastcampus.boardserver.mapper.PostMapper;
+import com.fastcampus.boardserver.mapper.TagMapper;
 import com.fastcampus.boardserver.mapper.UserProfileMapper;
 import com.fastcampus.boardserver.service.PostService;
 import lombok.extern.log4j.Log4j2;
@@ -22,6 +26,12 @@ public class PostServiceImpl implements PostService {
     @Autowired
     private UserProfileMapper userProfileMapper;
 
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @Autowired
+    private TagMapper tagMapper;
+
     @Override
     public void register(String id, PostDTO postDTO) {
         UserDTO memberInfo = userProfileMapper.getUserProfile(id);
@@ -30,6 +40,15 @@ public class PostServiceImpl implements PostService {
 
         if (memberInfo != null) {
             postMapper.register(postDTO);
+            Integer postId = postDTO.getId();
+            // 생성된 post 객체 에서 태그 리스트 생성
+            for(int i=0; i<postDTO.getTagDTOList().size(); i++){
+                TagDTO tagDTO = postDTO.getTagDTOList().get(i);
+                tagMapper.register(tagDTO);
+                Integer tagId = tagDTO.getId();
+                // M:N 관계 테이블 생성
+                tagMapper.createPostTag(tagId, postId);
+            }
         } else {
             log.error("register ERROR! {}", postDTO);
             throw new RuntimeException("register ERROR! 게시글 등록 메서드를 확인해주세요" + postDTO);
@@ -59,6 +78,66 @@ public class PostServiceImpl implements PostService {
         } else {
             log.error("deletePosts ERROR! {}", postId);
             throw new RuntimeException("deletePosts ERROR! 게시글 삭제 메서드를 확인해주세요" + postId);
+        }
+    }
+
+    @Override
+    public void registerComment(CommentDTO commentDTO) {
+        if (commentDTO.getPostId() != 0) {
+            commentMapper.register(commentDTO);
+        } else {
+            log.error("registerComment ERROR! {}", commentDTO);
+            throw new RuntimeException("registerComment" + commentDTO);
+        }
+    }
+
+    @Override
+    public void updateComment(CommentDTO commentDTO) {
+        if (commentDTO != null) {
+            commentMapper.updateComments(commentDTO);
+        } else {
+            log.error("updateComment ERROR!");
+            throw new RuntimeException("updateComment");
+        }
+    }
+
+    @Override
+    public void deletePostComment(int userId, int commentId) {
+        if (userId != 0 && commentId != 0) {
+            commentMapper.deletePostComment(commentId);
+        } else {
+            log.error("deletePostComment ERROR! {}", commentId);
+            throw new RuntimeException("deletePostComment" + commentId);
+        }
+    }
+
+    @Override
+    public void registerTag(TagDTO tagDTO) {
+        if (tagDTO.getPostId() != 0) {
+            tagMapper.register(tagDTO);
+        } else {
+            log.error("registerTag ERROR! {}", tagDTO);
+            throw new RuntimeException("registerTag ERROR! 태그 추가 메서드를 확인해주세요\n" + "Params : " + tagDTO);
+        }
+    }
+
+    @Override
+    public void updateTag(TagDTO tagDTO) {
+        if (tagDTO != null) {
+            tagMapper.updateTags(tagDTO);
+        } else {
+            log.error("updateTag ERROR!");
+            throw new RuntimeException("updateTag ERROR!");
+        }
+    }
+
+    @Override
+    public void deletePostTag(int userId, int tagId) {
+        if (userId != 0 && tagId != 0) {
+            tagMapper.deletePostTag(tagId);
+        } else {
+            log.error("deletePostTag ERROR!");
+            throw new RuntimeException("deletePostTag ERROR!");
         }
     }
 }
